ed90ed03f04c60069fcd2377d1ed4adf5adf8df7
Jhonny Scerni Gondim Costa <jhonny.costa@maps.com.br>
2020-12-26 22:38:50 -0300

    Ajuste no token

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService extends BaseService {
   getGrupo() {
     const token = this.obterToken();
     if (token) {
-      const grupo = this.jwtHelper.decodeToken(token).grupo
+      const grupo = this.jwtHelper.decodeToken(token).tipo
       return grupo;
     }
     return null;

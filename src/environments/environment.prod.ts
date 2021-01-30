8c41c2669acd1bf5544cc580c13235be5c3bde0b
jhonnyscerni <jhonnyscerni@gmail.com>
2021-01-30 20:28:14 -0300

    ajustes finos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-175-65-66.compute-1.amazonaws.com',
-  imagensUrl: 'http://ec2-54-175-65-66.compute-1.amazonaws.com',
+  apiUrl: 'http://ec2-18-215-170-179.compute-1.amazonaws.com',
+  imagensUrl: 'http://ec2-18-215-170-179.compute-1.amazonaws.com',
   clientId: 'projeto-web',
   clientSecret: 'web123',
   obterTokenUrl: '/oauth/token'
